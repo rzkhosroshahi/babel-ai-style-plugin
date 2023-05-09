4a46bf83b70cd9913a7f747c4842f768b33be1a9
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2023-05-09 17:31:07 +0330

    chore: remove unused module

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 // babel-ai-style-plugin
 module.exports = function (babel) {
   return {
-    visitor: {
-      TemplateLiteral: function (path) {
-        console.log('path >>', path);
-      }
-    },
+    visitor: {},
   }
 }
